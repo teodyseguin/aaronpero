a22b832205aacff4046bdf5009f9288e52121e6c
Teody C. Seguin <teodyseguin@gmail.com>
2021-01-29 05:54:15 +0800

    chore: update theme particle

---
+++ web/themes/custom/particle/dist/app-pl/pl/patterns/01-atoms-demo-star-svg-star-svgs/atoms-demo-star-svgs.js
@@ -0,0 +1,13 @@
+/**
+ * Demo of star-svg. Pulls in star-svg assets, and provides demo-only assets.
+ *
+ * (This file is NOT imported by the design system, but is included as part of
+ * a Pattern Lab app.)
+ */
+
+// Import component assets
+import 'atoms/star-svg';
+
+// Import demo assets
+import './star-svgs.twig';
+import './star-svgs.yml';
---
+++ web/themes/custom/particle/source/default/_patterns/01-atoms/star-svg/__tests__/star-svg.test.js
@@ -0,0 +1,5 @@
+import { name } from '..';
+
+test('star-svg component is registered', () => {
+  expect(name).toBe('star-svg');
+});
